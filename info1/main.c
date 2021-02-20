c123bcd7e348c90d3839a58a67f395ec90f842b8
garrati <enrigarrapa@gmailcom>
2021-02-20 19:41:46 +0100

    creata cartella

---
+++ info1/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int *a = malloc(3 * sizeof(int));
+    for (int i = 0; i < 3; i++)
+    {
+        a[i] = i;
+    }
+
+    FILE *f = fopen("prova.txt", "w");
+    printf("hello world");
+    return 0;
+}
--- main.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{int a=0;
-    printf("hello world");
-    return 0;
-}
