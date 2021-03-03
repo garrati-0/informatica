26e8eac2595d05432a94ffeab84ca82ce36dde6d
garrati <enrigarrapa@gmailcom>
2021-03-03 18:02:21 +0100

    ESEMPIO 2

--- info2/esempioricorsione.c
+++ info2/esempioricorsione.c
@@ -3,14 +3,23 @@
 int fattoriale(int n)
 {
     if(n==1)
-    return 1;
+    return 1;//CASO BASE
     else
-    return n*fattoriale(n-1);
+    return n*fattoriale(n-1); // RICORSIONE
+}
+
+int sommafinoa(int n)
+{
+    if(n==1)
+    return 1; //caso base 
+    else 
+    return sommafinoa(n-1)+n;//ricorsione
 }
 
 int main(void)
 {
     
    int c= fattoriale(3);
+   c=sommafinoa(4);
     return 0;
 }
