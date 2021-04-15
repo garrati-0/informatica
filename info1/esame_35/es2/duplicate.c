d36e17df23790a52aa4e83f7a52eb27db3fdeea4
garrati <enrigarrapa@gmailcom>
2021-04-15 20:45:36 +0200

    es 2 esame 35

--- info1/esame_35/es2/duplicate.c
+++ info1/esame_35/es2/duplicate.c
@@ -1,13 +1,40 @@
 #include"stdbool.h"
+#include "stdlib.h"
 
 bool cifre_duplicate_hex(unsigned int n)
 {
-    return true;
+    char *v = NULL;
+	int i = 0;
+
+	while (n >= 1)//scomponiano il numero in base 16
+	{
+		v = realloc(v, (i + 1) * sizeof(int));
+		v[i] = n % 16;
+		n = n / 16;
+		i++;
+	}
+
+    for(int a=0;a<i;a++)
+    {
+        for(int k=0;k<i;k++)
+        {
+            if(i!=k)
+            {
+                if(v[i]==v[k])
+                {
+                    free(v);
+                    return true;
+                }
+            }
+        }
+    }
+
+    return false;
 }
 
 
  int main(void)
  {
-     cifre_duplicate_hex(5887);
+     cifre_duplicate_hex(10);
      return 0;
  }
