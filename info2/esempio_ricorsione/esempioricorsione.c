2ea1fb07c083abee87a64a149a695310b2b0b17e
garrati <enrigarrapa@gmailcom>
2021-04-15 11:49:56 +0200

    esempio ricorsione massimo comune divisore

--- info2/esempio_ricorsione/esempioricorsione.c
+++ info2/esempio_ricorsione/esempioricorsione.c
@@ -2,26 +2,39 @@
 
 int fattoriale(int n)
 {
-    if(n==1)
-    return 1;//CASO BASE
+    if (n == 1)
+        return 1; //CASO BASE
     else
-    return n*fattoriale(n-1); // RICORSIONE
+        return n * fattoriale(n - 1); // RICORSIONE
 }
 
 int sommafinoa(int n)
 {
-    if(n==1)
-    return 1; //caso base 
-    else 
-    return sommafinoa(n-1)+n;//ricorsione
+    if (n == 1)
+        return 1; //caso base
+    else
+        return sommafinoa(n - 1) + n; //ricorsione
 }
 
-
+int mcd(int m, int n)//esempio di ricorsione dove il risultato viene
+{                    //sintetizzaato via via che le chiamate ricorsive
+    if (m == n)      //sisuccendono
+    {
+        return m;
+    }else{
+        if(m>n)
+        {
+            return mcd(m-n,n);
+        }else{
+            return mcd(m,n-m);
+        }
+    }
+}
 
 int main(void)
 {
-    
-   int c= fattoriale(3);
-   c=sommafinoa(4);
+
+    int c = fattoriale(3);
+    c = sommafinoa(4);
     return 0;
 }
