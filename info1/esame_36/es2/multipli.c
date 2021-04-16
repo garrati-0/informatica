9caca14656d1013a5585bfd191595a22a07f1d4b
garrati <enrigarrapa@gmailcom>
2021-04-16 19:17:40 +0200

    es 2 esame 36 corretto

--- info1/esame_36/es2/multipli.c
+++ info1/esame_36/es2/multipli.c
@@ -2,30 +2,40 @@
 
 int *rimuovi_multipli(const int *v, size_t size, size_t *newsize)
 {
-    int *new = calloc(size,sizeof(int));
+    if (v == NULL)
+        return NULL;
+
+    int *new = calloc(size, sizeof(int));
     size_t a = 0;
+    size_t b = 0;
+
     for (size_t i = 0; i < size; i++)
     {
         for (size_t k = 0; k < size; k++)
         {
-            if ((v[k] % v[i]) == 0 && i != k)
+            if ((v[i] % v[k]) == 0 && i != k)
             {
-                if (v[i] != new[a-1])
-                {
-                    new[a] = v[i];
-                    a++;
-                }
+
+                break;
             }
+            b++;
         }
+        if (b == size)
+        {
+            new[a] = v[i];
+            a++;
+        }
+        b = 0;
     }
-    new = realloc(new, a* sizeof(int));
-    newsize = &a;
+    new = realloc(new, a * sizeof(int));
+    *newsize = a;
     return new;
 }
 
 int main(void)
 {
-    int vet[] = {10, 9, 8, 7, 6, 5, 4, 3, 2};
-    rimuovi_multipli(vet, 9, NULL);
+    int vet[] = {3, 4, 5, 6, 7, 8, 9, 10};
+    size_t *l = malloc(1 * sizeof(size_t));
+    rimuovi_multipli(vet, 8, l);
     return 0;
 }
