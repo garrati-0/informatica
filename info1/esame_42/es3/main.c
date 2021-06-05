44f1996c2ceb0c648896621dd042bfcfcf6c79e6
enrico <enrigarrapa@gmail.com>
2021-06-05 11:41:53 +0200

    esame 42

--- info1/esame_42/es3/main.c
+++ info1/esame_42/es3/main.c
@@ -2,6 +2,18 @@
 
 int main(void)
 {
-    struct point a;
+    struct point p1 = {1, 2};
+     struct point p4 = {3, 4};
+    struct point p2 = {-9, -8};
+    struct point p3 = {-7, -6};
+   
+
+    struct rect *c = malloc(2 * sizeof(struct point));
+    c[0].a = p1;
+    c[0].b = p4;
+    c[1].a = p3;
+    c[1].b = p2;
+    
+   find_largest(c,2);
     return 0;
 }
--- info1/esame_42/es3/rectangle.c
+++ info1/esame_42/es3/rectangle.c
@@ -2,8 +2,66 @@
 
 extern int find_largest(const struct rect *r, size_t n)
 {
-    if(r==NULL||n==0)
-    return -1;
-    
-    return 0;
+    if (r == NULL || n == 0)
+        return -1;
+
+    int b = 0;
+    int h = 0;
+    int area = 0;
+    int tmp = 0;
+    int ris = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (r[i].a.x < 0)
+        {
+            b = -r[i].a.x + r[i].b.x;
+        }
+        else if (r[i].b.x < 0)
+        {
+            b = r[i].a.x - r[i].b.x;
+        }
+        if (r[i].a.x < 0 && r[i].b.x < 0)
+        {
+            if (r[i].a.x > r[i].b.x)
+            {
+                b = r[i].a.x - r[i].b.x;
+            }
+            else if (r[i].a.x < r[i].b.x)
+            {
+                b = r[i].b.x - r[i].a.x;
+            }
+        }
+
+        if (r[i].a.y < 0)
+        {
+            h = -r[i].a.y + r[i].b.y;
+        }
+        else if (r[i].b.y < 0)
+        {
+            h = r[i].a.y - r[i].b.y;
+        }
+        if (r[i].a.y < 0 && r[i].b.y < 0)
+        {
+
+          // r[i].a.y=-r[i].a.y; 
+
+            if (r[i].a.y > r[i].b.y)
+            {
+                h = r[i].a.y - r[i].b.y;
+            }
+            else if (r[i].a.y < r[i].b.y)
+            {
+                h = r[i].b.y - r[i].a.y;
+            }
+        }
+
+        tmp = b * h;
+        if (area < tmp)
+        {
+            ris = i;
+            area = tmp;
+        }
+    }
+
+    return ris;
 }
