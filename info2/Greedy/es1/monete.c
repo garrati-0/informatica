429408dc073e9d26ef8781db94fd0f2f244b8b03
enrico <enrigarrapa@gmail.com>
2021-08-26 17:03:57 +0200

    es1 algoritmi greedy

--- info2/Greedy/es1/monete.c
+++ info2/Greedy/es1/monete.c
@@ -1,14 +1,31 @@
 #include <stdlib.h>
+#include <string.h>
 
 int *monete(int *tagli, size_t tagli_size, int buget)
 {
-    if(buget <=0)
-    return NULL;
+    if (buget <= 0)
+        return NULL;
+
+
+    int *res = calloc(tagli_size, sizeof(int));
+
+    for(size_t i=0; i<tagli_size; i++)
+    {
+         res[i]=buget/tagli[i];
+         buget=buget%tagli[i];
+    }
+
+    return res;
 }
 
 int main(void)
 {
-    int tagli[] = {50, 20, 100, 5, 2, 1};
+    int tagli[] = {50, 20, 10, 5, 2, 1};
     int *res = monete(tagli, sizeof(tagli) / sizeof(int), 126); //sizeof(tagli)/sizeof(int) è il numero di elementi del vettore
     free(res);
 }
+/*
+int tagli[] = {50, 20, 6, 5, 1}; con buget=10;
+non avrei ottenuto la soluzione ottima {5,5} ma
+avrei ottenuto {6,1,1,1,1} la quale non è la soluzione migliore
+*/
