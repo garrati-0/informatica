d55e62a486fabc05b0ceacf1c02993b8440518a2
enrico <enrigarrapa@gmail.com>
2021-11-13 19:04:14 +0100

    fibonacci

---
+++ info2/libro/ricorsione/fibonacci/fibonacci.c
@@ -0,0 +1,16 @@
+int Fibonacci(int n)
+{
+    if (n == 0)
+        return 0;
+
+    if (n == 1)
+        return 1;
+
+    return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+
+int main(void)
+{
+    int c = Fibonacci(6);
+    return 0;
+}
